c0e2bfb4f5c4d6694013ff4713ef15f0bd192b26
Tara Karimi <t.karimi@medrick.info>
2024-01-25 17:18:31 +0330

    Handle camera movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float horizontalSpeed = 0f;
+    public float maxVerticalSpeed = 0.5f;
+    public float verticalSpeed = -0.5f;
     [SerializeField] private Rigidbody2D rb;
+    public bool goUp = false;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Camera mainCamera; 
 
     void Start()
     {
@@ -13,13 +18,24 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         MovePlayer();
+        UpdateCameraPosition();
     }
 
     void MovePlayer()
     {
-        //float horizontalInput = moveSpeed;//Input.GetAxis("Horizontal");
-        Vector2 moveDirection = new Vector2(moveSpeed, 0);
+        verticalSpeed = goUp ? 2 * maxVerticalSpeed : -maxVerticalSpeed;
+        
+        Vector2 moveDirection = new Vector2(horizontalSpeed, verticalSpeed);
         
         rb.AddForce(moveDirection);
+    }
+    
+    private void UpdateCameraPosition()
+    {
+        if (mainCamera != null)
+        {
+            Vector3 cameraPosition = new Vector3(transform.position.x, transform.position.y, mainCamera.transform.position.z);
+            mainCamera.transform.position = cameraPosition;
+        }
     }
 }
