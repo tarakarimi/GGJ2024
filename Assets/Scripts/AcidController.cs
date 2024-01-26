e9cfc774989cbfac8d1598838c87fc3126543881
AlirezaF80 <alirezafarzaneh138@gmail.com>
2024-01-26 11:31:34 +0330

    Add Acid Controller

---
+++ Assets/Scripts/AcidController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class AcidController : MonoBehaviour {
+    [SerializeField] private float normalSpeed = 1f;
+
+    void Update() {
+        transform.Translate(Vector3.up * (normalSpeed * Time.deltaTime));
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        // If the player collides with the acid, lose the game
+    }
+}
