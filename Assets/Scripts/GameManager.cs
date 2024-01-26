14f54c292d6dffc6d0bd3535507d529cbf19dc46
AlirezaF80 <alirezafarzaneh138@gmail.com>
2024-01-26 12:40:23 +0330

    Add Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public enum GameState {
+    PreStart,
+    Playing,
+    Won,
+    AcidLost,
+    BloodLost
+}
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance;
+    public GameState GameState { get; private set; }
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start() {
+        GameState = GameState.PreStart;
+    }
+
+    public void WinGame() {
+        if (GameState == GameState.Playing) return;
+        GameState = GameState.Won;
+    }
+
+    public void LoseGameByAcid() {
+        if (GameState == GameState.Playing) return;
+        GameState = GameState.AcidLost;
+    }
+
+    public void LoseGameByBlood() {
+        if (GameState == GameState.Playing) return;
+        GameState = GameState.BloodLost;
+    }
+}
